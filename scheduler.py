02db4fdf3fada81b254833ec579795ec2816786c
Suphawit <suphawit.won@gmail.com>
2023-10-28 15:35:37 +0700

    fixed url

--- scheduler.py
+++ scheduler.py
@@ -23,8 +23,8 @@ while True:
     msg = sub.get_message()
     if msg:
         print(f"new message in channel {msg['channel']}: {msg['data']}")
-        url_post_chunk = "http://backend:80/chunk"
-        url_post_thumbnail = "http://backend:80/thumbnail"
+        url_post_chunk = "http://backend:80/api/chunk"
+        url_post_thumbnail = "http://backend:80/api/thumbnail"
         requests.post(url_post_chunk, json=json.loads(msg['data']))
         requests.post(url_post_thumbnail, json=json.loads(msg['data']))
         time.sleep(10)
